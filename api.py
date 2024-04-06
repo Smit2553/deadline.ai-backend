fb08c7aa1b527e7ea1c5f6ef9a4aed2ac0100387
Smit Devrukhkar <smitdev2553@gmail.com>
2024-04-05 17:51:15 -0700

    Remove unnecessary API routes and update task storage logic

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import platform
 import sqlite3
+import src.tasks as Tasks
+
 app = FastAPI()
 
 # Routes
@@ -19,7 +21,12 @@ def read_root():
 
 @app.get("/weekly_tasks")
 def read_weekly_tasks():
-    conn = sqlite3.connect('../db/tasks.db')
+    try:
+        Tasks.store_weekly_tasks()
+    except Exception as e:
+        return {"error": str(e)}
+
+    conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
     c.execute('PRAGMA table_info(weekly_tasks)')
     columns = [column[1] for column in c.fetchall()]
@@ -34,7 +41,7 @@ def read_weekly_tasks():
 
 @app.get("/tasks")
 def read_all_tasks():
-    conn = sqlite3.connect('../db/tasks.db')
+    conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
     c.execute('PRAGMA table_info(all_tasks)')
     columns = [column[1] for column in c.fetchall()]
@@ -46,7 +53,7 @@ def read_all_tasks():
 
 @app.get("/tasks/{task_id}")
 def read_task(task_id: str):
-    conn = sqlite3.connect('../db/tasks.db')
+    conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
     c.execute('SELECT * FROM all_tasks WHERE id = ?', (task_id,))
     task = c.fetchone()
@@ -56,7 +63,7 @@ def read_task(task_id: str):
 # Update task status to completed
 @app.get("/tasks/complete/{task_id}")
 def complete_task(task_id: str):
-    conn = sqlite3.connect('../db/tasks.db')
+    conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
     c.execute('UPDATE all_tasks SET status = 1 WHERE id = ?', (task_id,))
     conn.commit()
@@ -66,7 +73,7 @@ def complete_task(task_id: str):
 # Update task status to incomplete
 @app.get("/tasks/incomplete/{task_id}")
 def incomplete_task(task_id: str):
-    conn = sqlite3.connect('../db/tasks.db')
+    conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
     c.execute('UPDATE all_tasks SET status = 0 WHERE id = ?', (task_id,))
     conn.commit()
--- src/tasks.py
+++ src/tasks.py
@@ -3,6 +3,15 @@ import datetime
 import sqlite3
 import uuid
 
+def clear_tasks():
+    conn = sqlite3.connect('db/tasks.db')
+    c = conn.cursor()
+    c.execute('DROP TABLE IF EXISTS weekly_tasks')
+    c.execute('DROP TABLE IF EXISTS all_tasks')
+    conn.commit()
+    conn.close()
+    return True
+
 def get_weekly_tasks():
     with open('weekly_tasks.ics', encoding='UTF-8') as f:
         cal = icalendar.Calendar.from_ical(f.read())
@@ -13,7 +22,11 @@ def get_weekly_tasks():
                 events = {
                     'date' : event.get('dtstart').dt.strftime('%Y-%m-%d'),
                     'summary' : event.get('summary'),
-                    'time' : event.get('dtstart').dt.strftime('%H:%M')
+                    'time' : event.get('dtstart').dt.strftime('%H:%M'),
+                    'description' : event.get('description'),
+                    'location' : event.get('location'),
+                    'id' : str(uuid.uuid4()),
+                    'status' : 0
                 }
                 event_list.append(events)
     except Exception as e:
@@ -22,21 +35,23 @@ def get_weekly_tasks():
     return event_list
 
 def store_weekly_tasks():
+    clear_tasks()
     conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS weekly_tasks (date TEXT, summary TEXT, time TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS weekly_tasks (date TEXT, summary TEXT, time TEXT, description TEXT, location TEXT, id TEXT, status INTEGER DEFAULT 0)')
     conn.commit()
     conn.close()
 
     for event in get_weekly_tasks():
         conn = sqlite3.connect('db/tasks.db')
         c = conn.cursor()
-        c.execute('INSERT INTO weekly_tasks (date, summary, time) VALUES (?, ?, ?)', (event['date'], event['summary'], event['time']))
+        c.execute('INSERT INTO weekly_tasks (date, summary, time, description, location, id, status) VALUES (?, ?, ?, ?, ?, ?, ?)', (event['date'], event['summary'], event['time'], event['description'], event['location'], event['id'], event['status'] ))
         conn.commit()
         conn.close()
     return True
 
 def get_all_tasks():
+
     with open('weekly_tasks.ics', encoding='UTF-8') as f:
         cal = icalendar.Calendar.from_ical(f.read())
     event_list = []
@@ -58,6 +73,7 @@ def get_all_tasks():
     return event_list
 
 def store_all_tasks():
+    clear_tasks()
     conn = sqlite3.connect('db/tasks.db')
     c = conn.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS all_tasks (date TEXT, summary TEXT, time TEXT, description TEXT, location TEXT, id TEXT, status INTEGER DEFAULT 0)')
